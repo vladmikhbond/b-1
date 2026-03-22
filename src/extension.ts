40594a56cf9c48109817ae90195ba4bc43834947
vladmikhbond <vladmikhbond@gmail.com>
2026-03-22 10:18:05 +0200

    .

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 import { loginCommand, disposeLogin } from './login_command';
 import { checkCommand, focus_spy, focus_spy2} from './check_command';
-import { helpCommand} from './help_command';
 
 export function activate(context: vscode.ExtensionContext) 
 {
@@ -9,8 +8,6 @@ export function activate(context: vscode.ExtensionContext)
     let disposable = vscode.commands.registerCommand('b1.login', loginCommand );
     context.subscriptions.push(disposable); 
 	disposable = vscode.commands.registerCommand('b1.check', checkCommand);
-	context.subscriptions.push(disposable);
-    disposable = vscode.commands.registerCommand('b1.help', () => helpCommand(context.extensionUri));
 	context.subscriptions.push(disposable);
     
     // event handlers
